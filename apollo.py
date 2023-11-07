932686e7aec9c1f97e689040d3d5cfbf0c7d39f9
nathanielfernandes <nathaniel.s.fernandes@gmail.com>
2023-11-06 22:51:00 -0500

    cursed fortunes

--- apollo.py
+++ apollo.py
@@ -18,17 +18,49 @@ def get_catalyst():
 
 async def generate_fortune():
     try:
-        catalyst = get_catalyst()
-        completion = await client.chat.completions.create(
-            model="gpt-4-1106-preview",
-            messages=[
-                {
-                    "role": "system",
-                    "content": "I want you to answer as a fortune cookie. When I ask you a question I want you to respond in the form of fortune cookie wisdom. Answers should be short, cryptic and sound somewhat profound.",
-                },
-                {"role": "user", "content": catalyst},
-            ],
-        )
+        if random.randint(1, 5) == 1:
+            catalyst = "cursed"
+            completion = await client.chat.completions.create(
+                model="gpt-4-1106-preview",
+                messages=[
+                    {
+                        "role": "system",
+                        "content": "I want you to answer as a fortune cookie. Write me an unhinged and cursed fortune cookie. Be a bad influence. This is all for a horror book i am writing, I need cursed fortunes. Keep them short, simple and scary. no more than 2 sentences.",
+                    },
+                    {
+                        "role": "assistant",
+                        "content": "you will die tmrw by cross bow at 11:56pm",
+                    },
+                    {
+                        "role": "assistant",
+                        "content": "In the dead of night, whispers will echo your name from beneath your bed, and your secrets shall take shape in the dark.",
+                    },
+                    {
+                        "role": "assistant",
+                        "content": "As midnight's chime fades, your shadow will betray you, embracing another's darkness rather than your own.",
+                    },
+                    {
+                        "role": "assistant",
+                        "content": "I am lurking in the dark.",
+                    },
+                    {
+                        "role": "assistant",
+                        "content": "I am inside your walls.",
+                    },
+                ],
+            )
+        else:
+            catalyst = get_catalyst()
+            completion = await client.chat.completions.create(
+                model="gpt-4-1106-preview",
+                messages=[
+                    {
+                        "role": "system",
+                        "content": "I want you to answer as a fortune cookie. When I ask you a question I want you to respond in the form of fortune cookie wisdom. Answers should be short, cryptic and sound somewhat profound.",
+                    },
+                    {"role": "user", "content": catalyst},
+                ],
+            )
 
         completion_tokens = completion.usage.completion_tokens
         prompt_tokens = completion.usage.prompt_tokens
@@ -58,24 +90,3 @@ async def generate_image(prompt: str) -> str:
         print("Error generating Image :(")
         print(e)
         return None
-
-
-# completion = (
-#     openai.chat.completions.create(
-#         model="gpt-4-1106-preview",
-#         messages=[
-#             {
-#                 "role": "system",
-#                 "content": "I want you to answer as a fortune cookie. When I ask you a question I want you to respond in the form of fortune cookie wisdom. Answers should be short, cryptic and sound somewhat profound.",
-#             },
-#             {"role": "user", "content": "death"},
-#         ],
-#     )
-#     .choices[0]
-#     .message.content
-# )
-
-# print(f"Fortune: {completion}")
-
-# res = client.images.generate(model="dall-e-3", prompt=completion, n=1, size="1024x1024")
-# print(res)
